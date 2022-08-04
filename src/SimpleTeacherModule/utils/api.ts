5190226225ae8bf9ed7a9b2acefc87fd3e207554
Cang Ta <cang.ta@kidsloop.live>
2022-08-04 20:04:56 +0700

    feat: chich choe

--- src/SimpleTeacherModule/utils/api.ts
+++ src/SimpleTeacherModule/utils/api.ts
@@ -1,19 +1,21 @@
-import axios from "axios";
-const basicPath = `${process.env.PUBLIC_URL}/dutch-lady`;
+import axios from 'axios';
+const basicPath = `${process.env.PUBLIC_URL}/chich-choe`;
 
 type Params = { [key: string]: string };
 const generateURLParams = (params: Params): string => {
-  const searchParams = new URLSearchParams();
-  for (const key in params) {
-    if (params.hasOwnProperty(key) && params[key]) {
-      searchParams.set(key, params[key]);
+    const searchParams = new URLSearchParams();
+    for (const key in params) {
+        if (params.hasOwnProperty(key) && params[key]) {
+            searchParams.set(key, params[key]);
+        }
     }
-  }
-  return searchParams.toString();
+    return searchParams.toString();
 };
 
 function geUnits(params: Params = {}) {
-  return axios.get(`${basicPath}/units.json?${generateURLParams(params)}`).then((res) => res.data);
+    return axios
+        .get(`${basicPath}/units.json?${generateURLParams(params)}`)
+        .then((res) => res.data);
 }
 
 // function getLessonPlan(unitid: string, params: Params = {}) {
@@ -21,19 +23,31 @@ function geUnits(params: Params = {}) {
 // }
 
 function getLessonPlan(params: Params = {}) {
-  return axios.get(`${basicPath}/units.json?${generateURLParams(params)}`).then((res) => res.data);
+    return axios
+        .get(`${basicPath}/units.json?${generateURLParams(params)}`)
+        .then((res) => res.data);
 }
 
 function geLessonMaterials(planid: string) {
-  return axios.get(`${basicPath}/plans/${planid}.json`).then((res) => res.data.materials);
+    return axios
+        .get(`${basicPath}/plans/${planid}.json`)
+        .then((res) => res.data.materials);
 }
 
 function getCurriculumData() {
-  return axios.get(`${basicPath}/curriculums.json`).then(res => res.data)
+    return axios.get(`${basicPath}/curriculums.json`).then((res) => res.data);
 }
 
-function getLessonPlans(level_id:string) {
-  return axios.get(`${basicPath}/levels/${level_id}.json`).then(res => res.data)
+function getLessonPlans(level_id: string) {
+    return axios
+        .get(`${basicPath}/levels/${level_id}.json`)
+        .then((res) => res.data);
 }
 
-export { geUnits, getLessonPlan, geLessonMaterials, getCurriculumData, getLessonPlans };
+export {
+    geUnits,
+    getLessonPlan,
+    geLessonMaterials,
+    getCurriculumData,
+    getLessonPlans,
+};
